674175473cfd4d9f5ab096a8f43a53c736aeabf4
zdungpham <dungphamvan973@gmail.com>
2024-04-04 20:55:18 +0700

    homeScreen

---
+++ lib/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "PDF TOOL",
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text('Choose your pdf file'),
+            ],
+          )
+          ),
+        ),
+      );
+  }
+}
